8343afc2ad24203c27fa4c4271402d5aa6c971c9
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-15 23:40:05 +0300

    2.2 Предварительные итоги

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 
+canvas.height = 700;
+canvas.width = 700;
+
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = '#2c2c2c';
 
