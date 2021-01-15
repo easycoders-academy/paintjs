46042e6c1dbd99e1b4fa99575063028af5983a9c
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-16 01:09:23 +0300

    2.3 Изменение цвета

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.getElementsByClassName('jsColor');
 
 canvas.height = 700;
 canvas.width = 700;
@@ -33,6 +34,11 @@ function onMouseDown(event){
     painting = true;
 }
 
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
 
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
@@ -40,3 +46,5 @@ if (canvas) {
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
