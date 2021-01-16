68195ef410d95063910f49a9c8139ca998ac0830
Anton Golosnichenko <golosnichenko@artics.ru>
2021-01-16 23:56:37 +0300

    2.6 Сохранение изображения

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const saveBtn = document.getElementById('jsSave');
 
 const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 700;
 
 canvas.height = CANVAS_SIZE;
 canvas.width = CANVAS_SIZE;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = INITIAL_COLOR;
@@ -68,6 +72,18 @@ function handleCanvasClick(){
     }
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = "PaintJS [Export]";
+    link.click();
+}
+
 
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
@@ -75,6 +91,7 @@ if (canvas) {
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
     canvas.addEventListener('click', handleCanvasClick);
+    canvas.addEventListener('contextmenu', handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener('click', handleColorClick));
@@ -85,4 +102,8 @@ if (range) {
 
 if (mode) {
     mode.addEventListener('click', handleModeClick);
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener('click', handleSaveClick);
 }
